3a1f474303681304d02faf87fea047f52ca692ca
bob <bob.dev.oak@gmail.com>
2025-10-02 21:13:13 -0700

    put back setup-jest.ts like it was in v19 - this is not the issue. Jest runs great one file at a time in v20 - but not with the standard  jest command ( npm run jest ). Need to specify which .spec.ts file you want to run.

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,21 +1,2 @@
-import { NgModule } from '@angular/core';
-import { TestBed } from '@angular/core/testing';
-import {
-  BrowserTestingModule,
-  platformBrowserTesting
-} from '@angular/platform-browser/testing';
-
-const providers: NgModule['providers'] = [];
-
-@NgModule({ providers })
-export class TestModule {}
-
-TestBed.initTestEnvironment(
-  [BrowserTestingModule, TestModule],
-  platformBrowserTesting(),
-  {
-    errorOnUnknownElements: true,
-    errorOnUnknownProperties: true
-  }
-);
-
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
+setupZoneTestEnv();
